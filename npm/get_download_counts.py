b028313fcd8f819bb2a8d2408157d1cd51bf33bf
Matthew Taylor <mjt@ku.edu>
2020-02-19 14:53:53 -0600

    Fixed npm download count getter

--- npm/get_download_counts.py
+++ npm/get_download_counts.py
@@ -2,6 +2,8 @@ import requests
 import threading
 import sys
 import time
+import os
+import json
 
 node_name = sys.argv[1]
 n_threads = 16
@@ -13,46 +15,37 @@ def chunks(lst, n):
         yield lst[i:i + n]
 
 all_packages = open('/users/m139t745/typosquatting/npm/cluster_input/{}'.format(node_name)).read().splitlines()
-
 log = open('/volatile/m139t745/npm_downloads/{}'.format(node_name), 'a')
-counter = 0
 downloads_url = 'https://api.npmjs.org/downloads/point/last-week/{}'
 
-def t(packages):
+def get_download_counts(packages):
 	for package_name in packages:
 		# get download counts
-		try:
+		r = requests.get(downloads_url.format(package_name))
+
+		while r.status_code == 429:
+			print('Package {} waiting, 429'.format(package_name))
+			time.sleep(10)
 			r = requests.get(downloads_url.format(package_name))
 
-			while r.status_code == 429:
-				print('Package {} waiting, 429'.format(package_name))
-				time.sleep(30)
-				r = requests.get(downloads_url.format(package_name))
+		if r.status_code == 200:
 
-			if r.status_code == 200:
+			weekly_downloads = r.json()['downloads']
 
-				weekly_downloads = r.json()['downloads']
+			lock.acquire()
+			log.write('{},{}\n'.format(package_name, weekly_downloads))
+			log.flush()
+			lock.release()
 
-				lock.acquire()
-				log.write('{},{}\n'.format(package_name, weekly_downloads))
-				counter += 1
-				if counter == 100:
-					log.flush()
-					counter = 0
-				lock.release()
-
-			else: 
-				
-				print('Package {} returned status code {}'.format(package_name, r.status_code))
-
-		except:
-			print('Unexpected error when processing {}'.format(package_name))
+		else: 
+			
+			print('Package {} returned status code {}'.format(package_name, r.status_code))
 
 threads = []
 all_chunks = chunks(all_packages, int(len(all_packages) / n_threads) + 1)
 for _ in range(n_threads):
 	current_chunk = next(all_chunks)
-	t = threading.Thread(target=t, args=(current_chunk,))
+	t = threading.Thread(target=get_download_counts, args=(current_chunk,))
 	t.start()
 	threads.append(t)
 

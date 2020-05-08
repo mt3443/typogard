3ad0734f38bcf8c03a7e9a2ce16f3f23e59820f5
Matthew Taylor <mjt@ku.edu>
2020-05-07 19:58:05 -0500

    rubygems deps

---
+++ rubygems/dependency_getter.py
@@ -0,0 +1,66 @@
+import time
+import requests
+import threading
+import sys
+
+node_name = sys.argv[1]
+n_threads = int(sys.argv[2])
+
+all_lines = open('/volatile/m139t745/rubygems/input/{}'.format(node_name)).read().splitlines()
+output = open('/volatile/m139t745/rubygems/output/{}'.format(node_name), 'a')
+lock = threading.Lock()
+
+def thread_target(lines):
+	for line in lines:
+		package = line.split()[0]
+		version = line.split()[1].replace('(', '').replace(')', '')
+
+		if package in preprocessed:
+			continue
+
+		dependencies = set([package])
+		todo = [package]
+
+		while len(todo) != 0:
+			package_name = todo.pop(0)
+
+			r = requests.get('https://api.rubygems.org/api/v1/dependencies.json?gems={}'.format(package_name))
+
+			while r.status_code == 429:
+				time.sleep(30)
+				r = requests.get('https://api.rubygems.org/api/v1/dependencies.json?gems={}'.format(package_name))
+
+			if r.status_code != 200:
+				print('DEPENDENCY REQUEST FOR {} RETURNED STATUS CODE {}'.format(package_name, r.status_code))
+			else:
+				try:
+					metadata = r.json()[0]
+					for d in metadata['dependencies']:
+						dependencies.add(d[0])
+						todo.append(d[0])
+				except:
+					print('Unhandled exception when processing {}'.format(package_name))
+		
+		lock.acquire()
+		output.write('{},{}\n'.format(package, ','.join(list(dependencies))))
+		output.flush()
+		lock.release()
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+all_chunks = chunks(all_lines, int(len(all_lines) / n_threads) + 1)
+
+threads = []
+for _ in range(n_threads):
+	current_chunk = next(all_chunks)
+	t = threading.Thread(target=thread_target, args=(current_chunk,))
+	t.start()
+	threads.append(t)
+
+for t in threads:
+	t.join()
+
+output.close()
---
+++ rubygems/get_dependencies.py
@@ -0,0 +1,68 @@
+# Looks for typosquatting in the transitive dependencies of all packages for PyPI
+
+import json
+import os
+import re
+import random
+
+# get all packages
+print('Loading packages...', flush=True)
+all_packages = open('/users/m139t745/typosquatting/data/rubygems_package_names').read().splitlines()
+random.shuffle(all_packages)
+
+print('Getting cluster info...', flush=True)
+os.system('scontrol show node > node_info')
+node_info = open('node_info', 'r').readlines()
+
+all_nodes = []
+
+current_node = ''
+
+for line in node_info:
+    if line == '\n':
+        all_nodes.append(current_node)
+        current_node = ''
+    else:
+        current_node += line
+
+os.system('rm node_info')
+
+unused_nodes = []
+
+for node in all_nodes:
+    if 'CPUAlloc=0' in node and 'NodeName=g' not in node and 'NodeName=m' not in node and 'amd' not in node:
+        unused_nodes.append(node)
+
+del all_nodes
+
+unused_nodes = unused_nodes[-120:]
+
+total_cores = 0
+total_packages = len(all_packages)
+nodes_cores = {}
+
+for node in unused_nodes:
+    name = re.findall(r'NodeName=(\S+)', node)[0]
+    n_cores = re.findall(r'CPUTot=(\d+)', node)[0]
+    nodes_cores[name] = n_cores
+    total_cores += int(n_cores)
+
+packages_per_core = int(total_packages / total_cores) + 1
+
+# assign packages
+print('Assigning packages...', flush=True)
+for node in nodes_cores:
+    packages_for_this_node = packages_per_core * int(nodes_cores[node])
+    f = open('/voaltile/m139t745/rubygems/input/{}'.format(node), 'w')
+    for _ in range(packages_for_this_node):
+        if (len(all_packages) == 0):
+            break
+        p = all_packages.pop()
+        f.write('{}\n'.format(p))
+    f.close()
+
+# start clients
+for node in nodes_cores:
+    os.system('srun -N 1 -n 1 -c {} -w {} --mem-per-cpu=2G -- python3 dependency_getter.py {} {} &'.format(nodes_cores[node], node, node, nodes_cores[node]))
+
+print('Job started')
